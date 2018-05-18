c088b40e7a3a1ae9a76312725ada16494802f2ea
Mladen Milosevic <mldnmilosevic@gmail.com>
2018-05-18 22:08:40 +0200

    Added some javascript functionality, needs fixing

--- mockup/scripts/main.js
+++ mockup/scripts/main.js
@@ -2,3 +2,11 @@
  * Author: Mladen Milosevic
  * Date: 18.05.2018
  */
+
+/*jshint esversion: 6 */
+
+ function writeToFile() {
+    let username = $('#username').val();
+
+    console.log(username); 
+ }
