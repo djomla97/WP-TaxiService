5ccd141238e6f6c4ca6752a6128a84eb625aced2
Mladen Milosevic <mldnmilosevic@gmail.com>
2018-05-21 11:22:53 +0200

    Added Web API

---
+++ TaxiServiceWebAPI/TaxiServiceWebAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace TaxiServiceWebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ TaxiServiceWebAPI/TaxiServiceWebAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TaxiServiceWebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
