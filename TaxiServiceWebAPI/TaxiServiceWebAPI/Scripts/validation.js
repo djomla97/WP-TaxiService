22c04e10b10323d2252dc1eaffe3ffcef4924b39
Mladen Milosevic <mldnmilosevic@gmail.com>
2018-06-20 14:28:36 +0200

    Added faster (near)real-time validation

---
+++ TaxiServiceWebAPI/TaxiServiceWebAPI/Scripts/validation.js
@@ -0,0 +1,121 @@
+﻿/*
+ * Author: Mladen Milosevic
+ * Date: 01.06.2018
+ * Description: Kao 'real-time' validacija unosa
+ */
+
+$(document).ready(function () {
+
+    $('#username-found').hide();
+    $('#email-found').hide();
+
+    // Username validacija
+    $('#username').on('input', function () {
+
+        let username = $(this).val();
+        
+        $.ajax({
+            method: "GET",
+            url: "api/validate/username?username="+username
+        }).done(function (response) {
+
+            //console.log("Response: " + response);
+
+            if (response === "Found") {
+                $('#username-check').show();
+                $('#username-check').addClass('not-available');
+                $('#username-check').text('Username is taken.');
+            } else {         
+                $('#username-check').hide();
+                $('#username-check').text('');
+                $('#username-check').removeClass('not-available');
+            }
+
+        });
+    });
+
+    // Email validacija
+    $('#email').on('input', function () {
+
+        let email = $(this).val();
+
+        $.ajax({
+            method: "GET",
+            url: "api/validate/email?email=" + email
+        }).done(function (response) {
+
+            //console.log("Response: " + response);
+
+            if (response === "Found") {
+                $('#email-check').show();
+                $('#email-check').addClass('not-available');
+                $('#email-check').text('Email is not available.');
+            } else {      
+                $('#email-check').hide();
+                $('#email-check').text('');
+                $('#email-check').removeClass('not-available');
+            }
+
+        });
+    });
+
+    // Edit email validacija
+    $('#editEmail').on('input', function () {
+
+        let email = $(this).val();
+
+        if (email !== $('#info-email')) {
+
+            $.ajax({
+                method: "GET",
+                url: "api/validate/email?email=" + email
+            }).done(function (response) {
+
+                //console.log("Response: " + response);
+
+                if (response === "Found") {
+                    $('#email-form p').show();
+                    $('#email-form p').addClass('not-available');
+                    $('#email-form p').text('Email is not available.');
+                } else {
+                    $('#email-form p').hide();
+                    $('#email-form p').text('');
+                    $('#email-form p').removeClass('not-available');
+                }
+
+            });
+        }
+    });
+
+    // Edit username validacija
+    $('#editUsername').on('input', function () {
+
+        let username = $(this).val();
+        console.log(username);
+
+        if (username !== $('#info-username')) {
+
+            $.ajax({
+                method: "GET",
+                url: "api/validate/username?username=" + username
+            }).done(function (response) {
+
+                //console.log("Response: " + response);
+
+                if (response === "Found") {
+                    $('#email-form p').show();
+                    $('#email-form p').addClass('not-available');
+                    $('#email-form p').text('Username is not available.');
+                } else {
+                    $('#email-form p').hide();
+                    $('#email-form p').text('');
+                    $('#email-form p').removeClass('not-available');
+                }
+
+            });
+        }
+    });
+
+
+
+});
